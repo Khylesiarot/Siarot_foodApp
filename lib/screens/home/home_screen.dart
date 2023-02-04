7d98e6e09f94adfbd81491344435790f6cd0c4f7
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-02-04 18:47:38 +0800

    starting project code

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:food_ui_kit/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
